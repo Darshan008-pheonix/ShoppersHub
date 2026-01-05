86754f002a91b8b88539a78a03f168af54d4aa65
Darshan008-pheonix <www.555darshanrdn@gmail.com>
2026-01-05 15:08:55 +0530

    entities

---
+++ Back-End/BookingService/src/main/java/com/sph/book/entity/PaymentType.java
@@ -0,0 +1,12 @@
+package com.sph.book.entity;
+
+public enum PaymentType {
+	
+	UPI,
+    CREDIT_CARD,
+    DEBIT_CARD,
+    NET_BANKING,
+    WALLET,
+    COD
+
+}
---
+++ Back-End/BookingService/src/main/java/com/sph/book/entity/Receipt.java
@@ -0,0 +1,34 @@
+package com.sph.book.entity;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.sph.util.model.Address;
+
+
+@Document
+public class Receipt {
+	
+	@Id
+    private String receiptId;
+
+    private String bookingId;
+    private String userId;
+
+    private PaymentType paymentType;
+    private PaymentStatus paymentStatus;
+    private String paymentTransactionId;
+
+    private double itemPrice;
+    private double taxAmount;
+    private double deliveryCharge;
+    private double totalAmount;
+
+    private Address billingAddress;
+    private Address deliveryAddress;
+
+    private Instant generatedAt;
+
+}
