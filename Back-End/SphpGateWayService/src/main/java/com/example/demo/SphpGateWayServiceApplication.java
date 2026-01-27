3d80bbcb09d94f3f72a41b1d77467bac3ac29853
Darshan008-pheonix <www.555darshanrdn@gmail.com>
2026-01-27 16:13:23 +0530

    api gateway1

--- Back-End/SphpGateWayService/src/main/java/com/example/demo/SphpGateWayServiceApplication.java
+++ Back-End/SphpGateWayService/src/main/java/com/example/demo/SphpGateWayServiceApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,18 @@ public class SphpGateWayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SphpGateWayServiceApplication.class, args);
+	}
+	
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p->p.path("/shp/payment/**")
+						.filters(f->f.rewritePath("/shp/payment/(?<segment>.*)", "/${segment}")
+						.retry(c->c.setRetries(3))
+						.circuitBreaker(c->c.setName("paymentServiceCircuitBreaker")
+						.setFallbackUri("forward:/paymentFallBack")))
+						.uri("lb://PAYMENTSERVICE"))
+				.build();
 	}
 
 }
